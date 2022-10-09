7f3b68d93ba003b6fbdc3a540fee76dc559122fb
SamiThueson <wea16002@byui.edu>
2022-10-08 23:29:44 -0600

    Most of it is done

---
+++ chamber/js/main.js
@@ -0,0 +1,24 @@
+document.querySelector("#yr").textContent = new Date(Date.now()).getFullYear();
+
+document.querySelector("#updated").textContent = `Last Modification: ${document.lastModified}`;
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// long, medium, short options ... try them
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
