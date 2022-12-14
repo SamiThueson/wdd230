cde6958263e42be36069307b0f4f3a785705bd5d
SamiThueson <wea16002@byui.edu>
2022-12-13 23:30:12 -0700

    final project started

---
+++ final_project/js/main.js
@@ -0,0 +1,9 @@
+document.querySelector("#yr").textContent = new Date(Date.now()).getFullYear();
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
